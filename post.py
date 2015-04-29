fbeb553859949b367fbbaf965bc03e7f502dda0c
gaopinghuang0 <gaopinghuang@gmail.com>
2015-04-28 20:58:00 -0400

    before run on AMT

--- post.py
+++ post.py
@@ -2,8 +2,8 @@ import crowdlib as cl, crowdlib_settings
 from main import URL_PREFIX 
 
 hit_type = cl.create_hit_type(
-	title = "CrowdChat",
-	description= "Please chat with requester and help them solve the proglems they have. This HIT is part of a class project at Purdue University by Gaoping Huang, Wu, Meng-Han, Jun Xiang Tee.  Feel free to email us at wu784@purdue.edu with any questions or concerns.  In case you run into any bugs, we will make sure you are paid. ",
+	title = "Find real need before shopping (bonus)",
+	description= "Please chat with requester and help to clarify his/her real need before purchasing certain merchandise.",
 	reward	= 0.4
 )
 
